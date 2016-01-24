72613ee2100a77f4bc879bd196357038ab55eb16
metulburr <micahpage911@yahoo.com>
2016-01-24 15:39:08 -0500

    fixed credits state

--- data/states/menu_states/credits.py
+++ data/states/menu_states/credits.py
@@ -5,16 +5,16 @@ from . import menus
 class Credits(menus.Menus):
     def __init__(self):
         menus.Menus.__init__(self)
-        self.options = ['Back']
+        self.options = ['']
         self.next_list = ['MENU']
         self.title_text = 'Credits'
         self.pre_render_options()
         self.setup_title()
         self.from_bottom = 500
     
-    def addition_event_handler(self, event):
-        #worked when first applied, but no longer effective for ?
+    def additional_event_handler(self, event):
         if event.type == pg.KEYDOWN or event.type == pg.MOUSEBUTTONDOWN:
+            #select hidden back button
             self.select_option(0)
         
         
