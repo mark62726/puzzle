f77114507c01c344e080ba11bf8c2c4538b87997
metulburr <micahpage911@yahoo.com>
2016-01-29 06:21:54 -0500

    moved unnecessary code from levels subclasses to game super class

--- data/levels/level2.py
+++ data/levels/level2.py
@@ -6,23 +6,16 @@ from ..states import game
 class Level2(game.Game):
     def __init__(self):
         game.Game.__init__(self)
-        #self.level_name = 'Level1'
         self.next = 'MENU'
         self.drop_boxes = [
             drop_box.DropBox(self.tile_rect.size, tools.from_center(self.screen_rect, (-400,-25))),
-            #drop_box.DropBox(self.tile_rect.size, tools.from_center(self.screen_rect, (-400,225))),
         ]
-        self.setup_text_flow()
-        self.setup_start_text() #update text to class name
         self.setup_end_text(pos=(300,650))
+        self.setup_text_flow()
         self.fill_tile_queue_order()
         self.tile_queue_layout()
-        self.controlled_drag = None
         self.fill_drop_box_layout()
         self.control_flow_order()
-        self.control_flow_index = 0
-        
-        self.control_pause = False
         
     def setup_text_flow(self):
         '''setup arbitrary texts for control flow'''
@@ -45,41 +38,12 @@ class Level2(game.Game):
             self.btn_dict['down_arrow'],
         ]
             
-    def tile_queue_layout(self):
-        '''starting layout of unselected tiles from the tile queue'''
-        self.tile_queue_spacer = 10
-        for i, obj in enumerate(self.tile_queue):
-            obj.rect.x = i*(obj.rect.width+self.tile_queue_spacer)
-            obj.rect.y = 0
-            obj.true_pos = list(obj.rect.center)
-            
     def fill_drop_box_layout(self):
         '''drop boxes that have tiles in them from start'''
         self.drop_box_queue = [
             self.btn_dict['turnaround_arrow']
         ]
         self.drop_boxes[0].set_occupant(self.drop_box_queue[0])
-        
-    def additional_get_event(self, event, keys):
-        if event.type == pg.KEYDOWN:
-            if prepare.DEBUG:
-                if event.key == pg.K_n:
-                    self.next = 'LEVEL2'
-                elif event.key == pg.K_p:
-                    self.next = 'LEVEL1'
-                self.done = True
-        for v in self.tile_queue:
-            v.get_event(event)
-        for tile in self.drop_box_queue:
-            tile.get_event(event)
-        for box in self.drop_boxes:
-            box.get_event(event, self.controlled_drag)
-            
-    def all_boxes_full(self):
-        for box in self.drop_boxes:
-            if box.empty:
-                return False
-        return True
             
     def update_control_arrow(self, now):
         '''pause/start control flow, change pause/start arrow color, and move arrow'''
@@ -101,37 +65,15 @@ class Level2(game.Game):
             self.control_paused = True
             
         if self.control_flow_index == len(self.drop_boxes)+2:
+            self.level_complete_sound.play()
             self.done = True
         
     def additional_update(self, now, keys, scale):
         self.update_control_arrow(now)
-        for box in self.drop_boxes:
-            box.update()
-        for drag in self.tile_queue:
-            drag.update(self.screen_rect, self.mouse_pos, scale)
-            if drag.click:
-                self.controlled_drag = drag
-        for tile in self.drop_box_queue:
-            tile.update(self.screen_rect, self.mouse_pos, scale)
-            if tile.click:
-                self.controlled_drag = tile
                 
     def additional_render(self, surface):
-        for box in self.drop_boxes:
-            box.render(surface)
-        for v in reversed(self.tile_queue):
-            v.render(surface)
-        for tile in self.drop_box_queue:
-            tile.render(surface)
+        pass
             
-        if not self.control_paused:
-            surface.blit(self.control_arrow, self.control_arrow_rect)
-        else:
-            surface.blit(self.control_arrow_paused, self.control_arrow_rect)
-            
-        for obj,rect in self.text_flow:
-            surface.blit(obj, rect)
-        
     def reset(self):
         self.control_flow_index = 0
         self.tile_queue_layout()
--- data/states/game.py
+++ data/states/game.py
@@ -16,6 +16,11 @@ class Game(state.State):
         self.control_paused = None
         self.level_complete_sound = prepare.SFX['positive']
         self.level_complete_sound.set_volume(.5)
+        self.tile_queue = []
+        self.control_flow_index = 0
+        self.controlled_drag = None
+        self.control_pause = False
+        self.setup_start_text()
         
     def get_level_num(self):
         return self.__class__.__name__[5:]
@@ -43,7 +48,7 @@ class Game(state.State):
         self.bg_orig = prepare.GFX['bg']
         self.bg = pg.transform.smoothscale(self.bg_orig, self.screen_rect.size)
         
-    def additional_get_event(self, event):
+    def additional_get_event(self, event, keys):
         '''subclass level get_event runs'''
         pass
         
@@ -54,6 +59,13 @@ class Game(state.State):
                 self.done = True
         self.music.get_event(event)
         self.additional_get_event(event, keys)
+        for v in self.tile_queue:
+            v.get_event(event)
+        for tile in self.drop_box_queue:
+            tile.get_event(event)
+        for box in self.drop_boxes:
+            box.get_event(event, self.controlled_drag)
+        
         
     def additional_update(self, now, keys, scale):
         '''subclass level updates'''
@@ -61,10 +73,17 @@ class Game(state.State):
         
     def update(self, now, keys, scale):
         pg.mouse.set_visible(True)
-        #if now-self.timer > 100:
-        #    self.timer = now
-            #print('sec')
         self.additional_update(now, keys, scale)
+        for box in self.drop_boxes:
+            box.update()
+        for drag in self.tile_queue:
+            drag.update(self.screen_rect, self.mouse_pos, scale)
+            if drag.click:
+                self.controlled_drag = drag
+        for tile in self.drop_box_queue:
+            tile.update(self.screen_rect, self.mouse_pos, scale)
+            if tile.click:
+                self.controlled_drag = tile
                 
     def additional_render(self):
         '''subclass level render'''
@@ -75,6 +94,34 @@ class Game(state.State):
         self.additional_render(surface)
         surface.blit(self.start_text, self.start_text_rect)
         surface.blit(self.end_text, self.end_text_rect)
+        for box in self.drop_boxes:
+            box.render(surface)
+        for v in reversed(self.tile_queue):
+            v.render(surface)
+        for tile in self.drop_box_queue:
+            tile.render(surface)
+            
+        if not self.control_paused:
+            surface.blit(self.control_arrow, self.control_arrow_rect)
+        else:
+            surface.blit(self.control_arrow_paused, self.control_arrow_rect)
+            
+        for obj,rect in self.text_flow:
+            surface.blit(obj, rect)
+
+    def tile_queue_layout(self):
+        '''starting layout of unselected tiles from the tile queue'''
+        self.tile_queue_spacer = 10
+        for i, obj in enumerate(self.tile_queue):
+            obj.rect.x = i*(obj.rect.width+self.tile_queue_spacer)
+            obj.rect.y = 0
+            obj.true_pos = list(obj.rect.center)
+            
+    def all_boxes_full(self):
+        for box in self.drop_boxes:
+            if box.empty:
+                return False
+        return True
         
     def cleanup(self):
         pass
